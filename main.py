50438d5afec947ca28a762d2c3b8ea518d86b6be
Artem <68923839+artem-phys@users.noreply.github.com>
2021-11-12 05:37:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+from qkdsimulationprotocol import QKDSimulationProtocol
+
+
+app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def qkd_protocol_run(
+    initial_key_length: int,
+    eavesdropping_rate: float,
+    error_estimation_sampling_rate: float,
+    error_reconciliation_efficiency: float
+):
+
+    qkd_protocol = QKDSimulationProtocol(initial_key_length, eavesdropping_rate, error_estimation_sampling_rate, error_reconciliation_efficiency)
+    qkd_protocol.run()
+    qkd_protocol.print_summary()
+
+    return JSONResponse(content=json.loads(qkd_protocol.output()))
